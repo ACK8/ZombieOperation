e01eb78445d88424c72bba2c8d423d4a08029c20
ACK202-07\UDON <m3568853679@neokidesu.click>
2016-12-12 15:14:59 +0900

    敵の巡回（完成）敵の視界（途中）

--- Assets/ZombieOperation/Scripts/Enemy/Enemy.cs
+++ Assets/ZombieOperation/Scripts/Enemy/Enemy.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
+    public float maxTimeToDiscover = 1.0f;
     public float[] waitTimes;
     public GameObject[] patrolObject;
+    public Transform eyePosition;
 
+    private bool isChase = false;
     private bool isPatrol = true;
     private bool isMove = true;
     private uint patrolNumber = 0;
+    private float timeToDiscover = 0.0f;
     private float currntWaitTime = 0.0f;
     private NavMeshAgent navMeshAgent;
+    private Transform playerTransform;
 
     void Start ()
     {
@@ -20,8 +25,13 @@ public class Enemy : MonoBehaviour
 	
 	void Update ()
     {
+        if(isChase)
+        {
+            navMeshAgent.SetDestination(playerTransform.position);
+        }
+
         //巡回処理
-        if(isPatrol)
+        if (isPatrol)
         {
             if (!isMove)
             {
@@ -53,6 +63,57 @@ public class Enemy : MonoBehaviour
         if (collider.gameObject.name == patrolObject[patrolNumber].name)
         {
             isMove = false;
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        //プレイヤーを発見
+        if (isChase) return;
+
+        Debug.Log("target = " + collider.gameObject.tag);
+
+        if (collider.gameObject.tag == "Player")
+        {
+            Debug.Log("Box発見");
+
+            RaycastHit hit;
+            Vector3 direction = collider.gameObject.transform.position - eyePosition.position;
+            if (Physics.Raycast(eyePosition.position, direction.normalized, out hit))
+            {
+                Debug.DrawRay(eyePosition.position, direction.normalized, Color.red);
+
+                if (hit.transform.tag == "Player")
+                {
+                    navMeshAgent.Stop();
+
+                    if (timeToDiscover > maxTimeToDiscover)
+                    {
+                        isChase = true;
+                        isMove = true;
+                        isPatrol = false;
+                        playerTransform = hit.transform;
+                        timeToDiscover = 0.0f;
+                        navMeshAgent.Resume();
+                    }
+                    else
+                    {
+                        timeToDiscover += Time.deltaTime;
+                    }
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (!isChase)
+        {
+            if (collider.gameObject.tag == "Player")
+            {
+                timeToDiscover = 0.0f;
+                navMeshAgent.Resume();
+            }
         }
     }
 }

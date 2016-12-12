62cc2e85be5d612163366599257b84680055c4e6
ACK202-07\UDON <m3568853679@neokidesu.click>
2016-12-12 11:42:39 +0900

    Enemy巡回（途中）

---
+++ Assets/ZombieOperation/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    public float[] waitTimes;
+    public Vector3[] patrolPositions;
+    public GameObject[] patrolObject;
+
+    private uint patrolNumber = 0;
+    private float currntWaitTime = 0;
+    private NavMeshAgent navMeshAgent;
+
+    void Start ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(patrolPositions[patrolNumber]);
+        currntWaitTime = waitTimes[patrolNumber];
+    }
+	
+	void Update ()
+    {
+        if(patrolNumber >= patrolPositions.Length)
+        {
+            patrolNumber = 0;
+        }
+
+        if(currntWaitTime >= waitTimes[patrolNumber])
+        {
+            navMeshAgent.SetDestination(patrolPositions[patrolNumber]);
+            currntWaitTime = 0.0f;
+        }
+        else
+        {
+            currntWaitTime += Time.deltaTime;   
+        }
+    }
+}
---
+++ Assets/ZombieOperation/Scripts/Enemy/TravelingPlace.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TravelingPlace : MonoBehaviour
+{
+
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}

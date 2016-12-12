f31e5038c96d4791f07a31e66239f17e977dd0af
ACK202-07\UDON <m3568853679@neokidesu.click>
2016-12-12 13:35:36 +0900

    no message

--- Assets/ZombieOperation/Scripts/Enemy/Enemy.cs
+++ Assets/ZombieOperation/Scripts/Enemy/Enemy.cs
@@ -4,35 +4,55 @@ using System.Collections;
 public class Enemy : MonoBehaviour
 {
     public float[] waitTimes;
-    public Vector3[] patrolPositions;
     public GameObject[] patrolObject;
 
+    private bool isPatrol = true;
+    private bool isMove = true;
     private uint patrolNumber = 0;
-    private float currntWaitTime = 0;
+    private float currntWaitTime = 0.0f;
     private NavMeshAgent navMeshAgent;
 
     void Start ()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        navMeshAgent.SetDestination(patrolPositions[patrolNumber]);
-        currntWaitTime = waitTimes[patrolNumber];
+        navMeshAgent.SetDestination(patrolObject[patrolNumber].transform.position);
     }
 	
 	void Update ()
     {
-        if(patrolNumber >= patrolPositions.Length)
+        //巡回処理
+        if(isPatrol)
         {
-            patrolNumber = 0;
-        }
+            if (!isMove)
+            {
+                //巡回位置切り替え
+                if (currntWaitTime > waitTimes[patrolNumber])
+                {
+                    patrolNumber += 1;
 
-        if(currntWaitTime >= waitTimes[patrolNumber])
-        {
-            navMeshAgent.SetDestination(patrolPositions[patrolNumber]);
-            currntWaitTime = 0.0f;
+                    if (patrolNumber >= patrolObject.Length)
+                    {
+                        patrolNumber = 0;
+                    }
+
+                    navMeshAgent.SetDestination(patrolObject[patrolNumber].transform.position);
+                    currntWaitTime = 0.0f;
+                    isMove = true;
+                }
+                else
+                {
+                    currntWaitTime += Time.deltaTime;
+                }
+            }
         }
-        else
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        //目的地に着いた
+        if (collider.gameObject.name == patrolObject[patrolNumber].name)
         {
-            currntWaitTime += Time.deltaTime;   
+            isMove = false;
         }
     }
 }

40d39008f693ec86081bb96c17fcf5f0a86bbe8e
ACKGame <m2127693073@neokidesu.click>
2016-12-13 15:03:10 +0900

    攻撃判定追加（途中）

---
+++ Assets/ZombieOperation/Scripts/Enemy/BulletFire.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFire : MonoBehaviour
+{
+    public GameObject firePoint;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float x = Random.Range(0.05f, -0.05f);
+            float y = Random.Range(0.05f, -0.05f);
+            float z = Random.Range(0.05f, -0.05f);
+
+            Vector3 dir = new Quaternion(x, y, z, 1.0f) * firePoint.transform.forward;
+
+            Vector3 start = firePoint.transform.position;
+            Vector3 end = start + dir * 5;
+
+            Debug.DrawLine(start, end);
+        }
+	}
+}
--- Assets/ZombieOperation/Scripts/Enemy/Enemy.cs
+++ Assets/ZombieOperation/Scripts/Enemy/Enemy.cs
@@ -1,21 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum EEnemyState
+{
+    None,
+    Wait,
+    Patrol,
+    Chase,
+    Attack,
+}
+
 public class Enemy : MonoBehaviour
 {
-    public float maxTimeToDiscover = 1.0f;
-    public float[] waitTimes;
-    public GameObject[] patrolObject;
-    public Transform eyePosition;
+    public float maxTimeToDiscover = 1.0f; //ターゲットを発見して認識する最大時間
+    public float[] waitTimes; //待機の最大時間
+    public string[] targetTags; //ターゲットのタグ名
+    public GameObject[] patrolObject; //巡回位置
+    public Transform eyePosition; //視界の位置
 
-    private bool isChase = false;
-    private bool isPatrol = true;
-    private bool isMove = true;
+    //銃関係
+    public float bulletDistanceMax = 5.0f; //銃弾の最大射程
+    public Transform firePoint; //銃口の位置
+
     private uint patrolNumber = 0;
     private float timeToDiscover = 0.0f;
     private float currntWaitTime = 0.0f;
     private NavMeshAgent navMeshAgent;
     private Transform playerTransform;
+    private EEnemyState enemyState = EEnemyState.Patrol;
 
     void Start ()
     {
@@ -25,80 +37,169 @@ public class Enemy : MonoBehaviour
 	
 	void Update ()
     {
-        if(isChase)
-        {
-            navMeshAgent.SetDestination(playerTransform.position);
-        }
+        Debug.Log(enemyState);
 
-        //巡回処理
-        if (isPatrol)
+        switch (enemyState)
         {
-            if (!isMove)
-            {
+            case EEnemyState.None:
+                break;
+
+            case EEnemyState.Wait:
                 //巡回位置切り替え
                 if (currntWaitTime > waitTimes[patrolNumber])
                 {
                     patrolNumber += 1;
-
-                    if (patrolNumber >= patrolObject.Length)
-                    {
-                        patrolNumber = 0;
-                    }
+                    if (patrolNumber >= patrolObject.Length) patrolNumber = 0;
 
                     navMeshAgent.SetDestination(patrolObject[patrolNumber].transform.position);
                     currntWaitTime = 0.0f;
-                    isMove = true;
+                    enemyState = EEnemyState.Patrol;
                 }
                 else
                 {
                     currntWaitTime += Time.deltaTime;
                 }
-            }
+
+                break;
+
+            case EEnemyState.Patrol:
+
+                break;
+
+            case EEnemyState.Chase:
+                navMeshAgent.SetDestination(playerTransform.position);
+                navMeshAgent.Resume();
+
+                //攻撃状態に移行
+                if (Vector3.Distance(playerTransform.position, firePoint.position) < bulletDistanceMax * 0.5f)
+                {
+                    //enemyState = EEnemyState.Attack;
+
+                    RaycastHit hit;
+                    Vector3 targetDir = (playerTransform.position - eyePosition.position).normalized;
+                    if (Physics.Raycast(eyePosition.position, targetDir, out hit))
+                    {
+                        Debug.Log("hit" + hit.transform.GetInstanceID());
+                        Debug.Log("playerTransform" + playerTransform.GetInstanceID());
+
+                        if (hit.transform.GetInstanceID() == playerTransform.GetInstanceID())
+                        {
+                            enemyState = EEnemyState.Attack;
+                        }
+                    }
+                }
+
+                break;
+
+            case EEnemyState.Attack:
+                {
+                    navMeshAgent.SetDestination(playerTransform.position);
+
+                    NavMeshHit navMeshHit2;
+                    if (!navMeshAgent.Raycast(playerTransform.position, out navMeshHit2))
+                    {
+                        enemyState = EEnemyState.Chase;
+                        break;
+                    }
+
+                    navMeshAgent.Stop();
+
+
+
+                    if (Vector3.Distance(playerTransform.position, firePoint.position) > bulletDistanceMax)
+                    {
+                        RaycastHit hit;
+                        Vector3 targetDir = (playerTransform.position - eyePosition.position).normalized;
+
+                        Debug.DrawLine(eyePosition.position, (eyePosition.position + targetDir));
+
+                        if (Physics.Raycast(eyePosition.position, targetDir, out hit))
+                        {
+                            if (hit.transform.tag != playerTransform.tag)
+                            {
+                                //enemyState = EEnemyState.Chase;
+                            }
+                        }
+                    }
+                }
+
+
+                /*
+                Vector3 direction = playerTransform.position - eyePosition.position;
+
+                if (Vector3.Distance(playerTransform.position, eyePosition.position) < 5.0f)
+                {
+                    float x = Random.Range(0.05f, -0.05f);
+                    float y = Random.Range(0.05f, -0.05f);
+                    float z = Random.Range(0.05f, -0.05f);
+
+                    Vector3 targetDir = (playerTransform.position - firePoint.position).normalized;
+                    Vector3 dir = new Quaternion(x, y, z, 1.0f) * targetDir;
+
+                    Vector3 start = firePoint.position;
+                    Vector3 end = start + dir * bulletDistanceMax;
+
+                    Debug.DrawLine(start, end);
+
+                    RaycastHit hitfire;
+                    if (Physics.Raycast(eyePosition.position, direction.normalized, out hitfire))
+                    {
+                    }
+                }
+                else
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(eyePosition.position, direction.normalized, out hit))
+                    {
+                    }
+                }
+                */
+                break;
         }
     }
 
     void OnTriggerEnter(Collider collider)
     {
+        if (!(enemyState == EEnemyState.Patrol)) return;
+
         //目的地に着いた
         if (collider.gameObject.name == patrolObject[patrolNumber].name)
         {
-            isMove = false;
+            enemyState = EEnemyState.Wait;
         }
     }
 
     void OnTriggerStay(Collider collider)
     {
-        //プレイヤーを発見
-        if (isChase) return;
-
-        Debug.Log("target = " + collider.gameObject.tag);
-
-        if (collider.gameObject.tag == "Player")
+        //視界処理
+        if((enemyState == EEnemyState.Patrol) || (enemyState == EEnemyState.Wait))
         {
-            Debug.Log("Box発見");
-
-            RaycastHit hit;
-            Vector3 direction = collider.gameObject.transform.position - eyePosition.position;
-            if (Physics.Raycast(eyePosition.position, direction.normalized, out hit))
+            for (int i = 0; i < targetTags.Length; i++)
             {
-                Debug.DrawRay(eyePosition.position, direction.normalized, Color.red);
-
-                if (hit.transform.tag == "Player")
+                if (collider.gameObject.tag == targetTags[i])
                 {
-                    navMeshAgent.Stop();
+                    RaycastHit hit;
+                    Vector3 direction = collider.gameObject.transform.position - eyePosition.position;
+                    if (Physics.Raycast(eyePosition.position, direction.normalized, out hit))
+                    {
+                        //Debug.DrawRay(eyePosition.position, direction.normalized, Color.red);
 
-                    if (timeToDiscover > maxTimeToDiscover)
-                    {
-                        isChase = true;
-                        isMove = true;
-                        isPatrol = false;
-                        playerTransform = hit.transform;
-                        timeToDiscover = 0.0f;
-                        navMeshAgent.Resume();
-                    }
-                    else
-                    {
-                        timeToDiscover += Time.deltaTime;
+                        if (hit.transform.tag == targetTags[i])
+                        {
+                            navMeshAgent.Stop();
+
+                            if (timeToDiscover > maxTimeToDiscover)
+                            {
+                                enemyState = EEnemyState.Chase;
+                                playerTransform = hit.transform;
+                                timeToDiscover = 0.0f;
+                                navMeshAgent.Resume();
+                            }
+                            else
+                            {
+                                timeToDiscover += Time.deltaTime;
+                            }
+                        }
                     }
                 }
             }
@@ -107,12 +208,15 @@ public class Enemy : MonoBehaviour
 
     void OnTriggerExit(Collider collider)
     {
-        if (!isChase)
+        if (!(enemyState == EEnemyState.Chase))
         {
-            if (collider.gameObject.tag == "Player")
+            for (int i = 0; i < targetTags.Length; i++)
             {
-                timeToDiscover = 0.0f;
-                navMeshAgent.Resume();
+                if (collider.gameObject.tag == targetTags[i])
+                {
+                    timeToDiscover = 0.0f;
+                    navMeshAgent.Resume();
+                }
             }
         }
     }

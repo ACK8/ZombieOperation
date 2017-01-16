1f0a4b00f558b8ed90106232850898f65c56ba45
ACK202_08 <m3568853679@neokidesu.click>
2017-01-16 15:05:16 +0900

    隔壁

---
+++ Assets/ZombieOperation/Scripts/Event/Door/Bulkhead.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bulkhead : MonoBehaviour
+{
+    public Transform pos;
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        print(transform.position);
+    }
+    public void OpenBulkhead(float speed)
+    {
+        transform.position=new Vector3(0, transform.position.y+speed, 0);
+    }
+}

6a23ef31a79da146d05e82d62bc6d771703c110c
ACK202_08 <m3568853679@neokidesu.click>
2017-01-16 11:46:08 +0900

    生体認証

--- Assets/ZombieOperation/Scripts/Event/Door/Biometrics.cs
+++ Assets/ZombieOperation/Scripts/Event/Door/Biometrics.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class Biometrics : MonoBehaviour
 {
+    public Door doorScript;
+    private Zombie zombie;
+    public int MachineID;
+    private bool isTouched=false;
+    private bool isMove=false;
 	void Start ()
     {
 	
@@ -10,6 +15,27 @@ public class Biometrics : MonoBehaviour
 
 	void Update ()
     {
-	
+        if(zombie)
+        {
+            if(zombie.isAuthenticationComplete&&!isMove)
+            {
+                isMove = true;
+                doorScript.MoveDoor();
+            }
+        }
 	}
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.tag=="Zombie")
+        {
+            if (MachineID == hit.GetComponent<Zombie>().zombieID && !isTouched)
+            {
+                isTouched = true;
+                hit.GetComponent<Zombie>().StartBiometrics();
+                zombie = hit.GetComponent<Zombie>();
+                print("Opresadas");
+
+            }
+        }
+    }
 }

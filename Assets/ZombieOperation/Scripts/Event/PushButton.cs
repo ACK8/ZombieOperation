2696fecdf2793a71cf18b2d000094aa62d68eba7
ken60 <kenken60826@gmail.com>
2017-01-18 14:37:13 +0900

    PushBadd utton

---
+++ Assets/ZombieOperation/Scripts/Event/PushButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PushButton : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent pushEvent;
+
+    private float count = 0f;
+    private bool isTouched = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //カードをタッチしたらカウント開始
+        if (isTouched)
+        {
+            count += Time.deltaTime;
+        }
+
+        //時間が来たら再びタッチできるように
+        if (1.5f < count)
+        {
+            count = 0f;
+            isTouched = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider hit)
+    {
+
+        if (hit.tag == "Key")
+        {
+        }
+    }
+}

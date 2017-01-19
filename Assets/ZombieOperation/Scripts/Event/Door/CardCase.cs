58884cf11379652bc1fd3298c9a86fda185f8298
GameZonbi <h.282424kome@gmail.com>
2017-01-19 14:25:44 +0900

    カードケースのシャッター　開ける

---
+++ Assets/ZombieOperation/Scripts/Event/Door/CardCase.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardCase : MonoBehaviour
+{
+	[SerializeField]
+	private float MoveSpeed;
+
+	private bool isOpen=false;
+	private bool isflag = false;
+	public Vector3 Tagetpos;
+
+	void Start ()
+	{
+		
+	}
+		
+	public void MoveShutter()
+	{
+		isflag = true;
+	}
+
+	void Update ()
+	{
+		if(isflag)
+		{
+			if (!isOpen)
+			{
+				OpenShutter ();
+			}	
+		}
+	}
+
+	public void OpenShutter()
+	{
+		transform.position= new Vector3 (transform.position.x,transform.position.y-MoveSpeed*Time.deltaTime,transform.position.z);
+		if (Tagetpos.y >= transform.position.y) 
+		{
+			transform.position =new Vector3(transform.position.x,Tagetpos.y,transform.position.z);
+			isOpen = true;
+			isflag = false;
+		}
+	}
+}
---
+++ Assets/ZombieOperation/Scripts/Event/Door/CardCaseKey.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardCaseKey : MonoBehaviour 
+{
+	[SerializeField]
+	private CardCase CardCaseScript;
+	[SerializeField]
+	private int cardKeyID;
+	private bool isTouched=false;
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.tag == "Key")
+		{
+			if (cardKeyID == hit.GetComponent<Key> ().cardID&&!isTouched) 
+			{
+				isTouched = true;
+				CardCaseScript.MoveShutter();
+			}
+		}
+	}
+
+}

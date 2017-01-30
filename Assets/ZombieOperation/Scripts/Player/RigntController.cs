0e4ecbceea641f9cd67ce76f84a5ca8424ed880a
ACK202-07\UDON <m3568853679@neokidesu.click>
2017-01-30 10:41:45 +0900

    add stage

---
+++ Assets/ZombieOperation/Scripts/Player/RigntController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class RigntController : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedComponent;
+    private SteamVR_Controller.Device device;
+    private LineRenderer line;
+    private RaycastHit hit;
+    private Ray ray;
+
+    void Start()
+    {
+        trackedComponent = gameObject.transform.parent.GetComponent<SteamVR_TrackedObject>();
+        line = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        device = SteamVR_Controller.Input((int)trackedComponent.index);
+
+        ray.direction = transform.forward;
+        ray.origin = transform.position;
+
+        line.SetPosition(0, ray.origin);
+        line.SetPosition(1, ray.GetPoint(200));
+
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            Menu.Instance.SwitchDisplay();
+        }
+
+        if (Physics.Raycast(ray, out hit, 200.0f))
+        {
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                Menu.Instance.SelectMenu(hit.collider.name);
+            }
+        }
+
+        line.enabled = Menu.Instance.isDisplayed;
+    }
+}

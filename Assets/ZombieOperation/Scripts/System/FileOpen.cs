34363b7e5db0f739f6f91972198629d190a633b2
ken60 <kenken60826@gmail.com>
2017-02-02 12:30:35 +0900

    当たり判定修正、小物配置

---
+++ Assets/ZombieOperation/Scripts/System/FileOpen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+using System.Text;
+
+// ファイル読み込み
+public class FileOpen : MonoBehaviour
+{
+    public static string FileRead(string _fileName)
+    {
+        FileInfo fi = new FileInfo(Application.dataPath + "/Database/" + _fileName + ".txt");
+        string returnSt = "";
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(fi.OpenRead(), Encoding.UTF8))
+            {
+                returnSt = sr.ReadToEnd();
+            }
+        }
+        catch (Exception e)
+        {
+            print(e.Message);
+            returnSt = "READ ERROR: " + _fileName;
+        }
+
+        return returnSt;
+    }
+}
